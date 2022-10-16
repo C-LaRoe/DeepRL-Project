50dff8f102fe4f2f1ee014f5aeec4f0a7d91d764
LFletch1 <lgfletcher1@buffs.wtamu.edu>
2022-10-15 23:27:58 -0500

    Got the environment working and the models training. Not very successful right now

--- missile_defense_env.py
+++ missile_defense_env.py
@@ -1,9 +1,9 @@
-from xml.etree.ElementPath import xpath_tokenizer
-from missile_defense import X_DIM
 import numpy as np
 import gym
 from gym import spaces
 import pygame
+from stable_baselines3 import PPO, A2C
+from stable_baselines3.common.env_checker import check_env
 import math
 import time
 import random as rd
@@ -18,7 +18,7 @@ class Missile(pygame.sprite.Sprite):
             self.x_t = 0
             self.y_t = Y_DIM - 124
             self.theta = 45
-            self.v = 12
+            self.v = 3
         
         else:
             # Generate random trajectory in the environment for the target missile to take
@@ -31,7 +31,7 @@ class Missile(pygame.sprite.Sprite):
                 self.theta = -(180/math.pi) * math.atan(Y_DIM/(destination_x - self.x_t))
             else:
                 self.theta = -90 + (180/math.pi) * math.atan((destination_x - self.x_t)/Y_DIM)
-            self.v = rd.randint(5,10)
+            self.v = rd.randint(6,8)
 
         self.image = pygame.image.load("sprites/missile3.png")
         self.small_image = pygame.transform.scale(self.image, (int(self.image.get_size()[0]/7),int(self.image.get_size()[1]/7)))
@@ -50,6 +50,7 @@ class Missile(pygame.sprite.Sprite):
         # Set collision box coordinates
         self.rect.x = self.x_t
         self.rect.y = self.y_t
+        self.rotate_image = pygame.transform.rotate(self.small_image, self.theta)
 
     def draw(self, screen):
         screen.blit(self.rotate_image, (self.x_t, self.y_t))
@@ -74,12 +75,12 @@ class MissileEnv(gym.Env):
         self.Y_DIM = Y_DIM
 
         pygame.init()
-        self.screen = pygame.display.set_mode[X_DIM, Y_DIM]
+        self.screen = pygame.display.set_mode([X_DIM, Y_DIM])
         self.background_color = (199, 250, 252)
         self.ground = Ground()
 
-        self.agent_missile = Missile()
-        self.target_missile = Missile()
+        self.agent_missile = Missile("agent")
+        self.target_missile = Missile("target")
         explosion_img = pygame.image.load("sprites/explosion.png")
         self.explosion_img = pygame.transform.scale(explosion_img, (int(explosion_img.get_size()[0]/10),int(explosion_img.get_size()[1]/10)))
         pygame.font.init()
@@ -90,36 +91,58 @@ class MissileEnv(gym.Env):
         self.action_space = spaces.Box(np.array([-2,-15]),np.array([2,15]))
         
         # State Space
-        self.observation_space = spaces.Dict(
-                            {
-                                "agent_x": spaces.Box(low=0, high=self.X_DIM, shape=()),
-                                "agent_y": spaces.Box(low=0, high=self.Y_DIM, shape=()),
-                                "agent_theta": spaces.Box(low=0, high=360, shape=()),
-                                "agent_v": spaces.Box(low=3, high=12, shape=()),
-                                "target_x": spaces.Box(low=0, high=self.X_DIM, shape=()),
-                                "target_y": spaces.Box(low=0, high=self.Y_DIM, shape=()),
-                                "target_theta": spaces.Box(low=0, high=360, shape=()),
-                                "target_v": spaces.Box(low=3, high=12, shape=())
-                            })
+        # self.observation_space = spaces.Dict(
+        #                     {
+        #                         "agent_x": spaces.Box(low=0, high=self.X_DIM, shape=()),
+        #                         "agent_y": spaces.Box(low=0, high=self.Y_DIM, shape=()),
+        #                         "agent_theta": spaces.Box(low=0, high=360, shape=()),
+        #                         "agent_v": spaces.Box(low=3, high=12, shape=()),
+        #                         "target_x": spaces.Box(low=0, high=self.X_DIM, shape=()),
+        #                         "target_y": spaces.Box(low=0, high=self.Y_DIM, shape=()),
+        #                         "target_theta": spaces.Box(low=0, high=360, shape=()),
+        #                         "target_v": spaces.Box(low=3, high=12, shape=())
+        #                     })
+        self.observation_space = spaces.Box(low=np.array([
+                                                            0,
+                                                            0,
+                                                            -360,
+                                                            3,
+                                                            0,
+                                                            0,
+                                                            -360,
+                                                            3
+                                                        ]).astype(np.float32),
+                                            high=np.array([
+                                                            self.X_DIM,
+                                                            self.Y_DIM,
+                                                            360,
+                                                            15,
+                                                            self.X_DIM,
+                                                            self.Y_DIM,
+                                                            360,
+                                                            12,
+                                                        ]).astype(np.float32)
+                                                    )
 
     def step(self, action):
-        print(action)       
+        # print(action)       
         # Need to update agent missile theta and v based on the action passed as a parameter
-        self.agent_missile.v += action[0] # Change v
         self.agent_missile.theta += action[1] # Change theta
-
+        if self.agent_missile.v + action[0] >= 3 and self.agent_missile.v + action[0] <= 15:
+            self.agent_missile.v += action[0] # Change v
 
         self.agent_missile.update_pos()
         self.target_missile.update_pos()
         self.agent_missile.draw(self.screen)
         self.target_missile.draw(self.screen)
         self.ground.draw(self.screen)
+        pygame.display.flip()
         # pygame.draw.rect(screen, (0,255,0) , target_missile.rect) # Draw collision box of target
 
         self.screen.fill(self.background_color)
         # If agent missile has hit ground or gone outside the bounds of the environment 
         if self.agent_missile.y_t > Y_DIM-124 or self.agent_missile.y_t < 0 or self.agent_missile.x_t > X_DIM or self.agent_missile.x_t < 0:
-            reward = -10
+            reward = -100
             done = True
 
         # Target missile hit ground
@@ -127,7 +150,7 @@ class MissileEnv(gym.Env):
             self.screen.blit(self.explosion_img, (self.target_missile.x_t-15, self.target_missile.y_t))
             pygame.display.flip()
             time.sleep(0.1)
-            reward = -10
+            reward = -100
             done = True
 
         # If there is a missile to missile collision
@@ -145,9 +168,67 @@ class MissileEnv(gym.Env):
 
         time.sleep(0.009)
         pygame.display.flip()
-        return np.array(), reward, done, {}
+
+        # observation = {
+        #             "agent_x": self.agent_missile.x_t,
+        #             "agent_y": self.agent_missile.y_t,
+        #             "agent_theta": self.agent_missile.theta,
+        #             "agent_v": self.agent_missile.v,
+        #             "target_x": self.target_missile.x_t,
+        #             "target_y": self.target_missile.y_t,
+        #             "target_theta": self.target_missile.theta,
+        #             "target_v": self.target_missile.v,
+        #         }
+        observation = np.array([self.agent_missile.x_t,self.agent_missile.y_t,self.agent_missile.theta,self.agent_missile.v,
+                                self.target_missile.x_t,self.target_missile.y_t,self.target_missile.theta,self.target_missile.v], dtype=np.float32)
+        return observation, reward, done, {}
 
     def reset(self):
         self.agent_missile = Missile(type="agent")
         self.target_missile = Missile(type="target")
-                                        
+        observation = np.array([self.agent_missile.x_t, self.agent_missile.y_t, self.agent_missile.theta, self.agent_missile.v,
+                                self.target_missile.x_t, self.target_missile.y_t, self.target_missile.theta, self.target_missile.v], dtype=np.float32)
+
+        # print(self.observation_space.shape)
+        # print(self.observation_space.dtype)
+        # print(observation)
+        return observation
+        # return {
+        #             "agent_x": self.agent_missile.x_t,
+        #             "agent_y": self.agent_missile.y_t,
+        #             "agent_theta": self.agent_missile.theta,
+        #             "agent_v": self.agent_missile.v,
+        #             "target_x": self.target_missile.x_t,
+        #             "target_y": self.target_missile.y_t,
+        #             "target_theta": self.target_missile.theta,
+        #             "target_v": self.target_missile.v,
+        #         }   
+
+
+def main():
+    env = MissileEnv()
+    # check_env(env)
+    t1 = time.time()
+    # model = A2C.load("a2c_missile")
+    model = A2C("MlpPolicy", env)
+    model.learn(total_timesteps=10000)
+    # model = A2C("MlpPolicy", env, verbose=1)
+    model.save("a2c_missile")
+    t2 = time.time()
+    print(t2 - t1, "seconds")
+
+    print("DONE TRAINING")
+
+    # del model # remove to demonstrate saving and loading
+
+    # model = PPO.load("a2c_missile")
+
+    # obs = env.reset()
+    # while True:
+    #     action, _states = model.predict(obs)
+    #     obs, rewards, done, info = env.step(action)
+    #     if done:
+    #         env.reset()
+
+if __name__ == "__main__":
+    main()
